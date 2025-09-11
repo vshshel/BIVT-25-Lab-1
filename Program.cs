8498b689e0b6899905f8405fb79f81741c626cec
Шелест Виктория <vika.shelest.07@bk.ru>
2025-09-11 14:32:24 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Diagnostics;
+
+namespace Lab1
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            White white = new White();
+            Console.WriteLine(white.Task1(1.2));
+            Console.WriteLine(white.Task2(2));
+            Console.WriteLine(white.Task3(2, 1));
+        }
+    }
+}   

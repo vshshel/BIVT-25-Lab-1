87a100db617b5c1e60ac785b5c729aaf40f3ec32
Шелест Виктория <vika.shelest.07@bk.ru>
2025-09-25 17:02:13 +0300

    Green League

--- Lab1/Green.cs
+++ Lab1/Green.cs
@@ -7,9 +7,9 @@
             bool answer = false;
 
             // code here
-
+            answer = (Math.Abs(d) >= 1.0000);
             // end
-
+            
             return answer;
         }
         public bool Task2(double d, double f)
@@ -17,7 +17,8 @@
             bool answer = false;
 
             // code here
-
+            double sr_z = (d + f) / 2;
+            answer = (sr_z > 0);
             // end
 
             return answer;
@@ -27,7 +28,9 @@
             bool answer = false;
 
             // code here
-
+            double sum = (double)a + b;
+            double sr_z = (double)Math.Abs(a) + Math.Abs(b) / 2;
+            answer = (sum > sr_z);
             // end
 
             return answer;
@@ -37,7 +40,8 @@
             int answer = 0;
 
             // code here
-
+            int maxx = Math.Max(a, b);
+            answer = Math.Max(c, maxx);
             // end
 
             return answer;
@@ -47,7 +51,14 @@
             double answer = 0;
 
             // code here
-
+            if (Math.Abs(x) > 1)
+            {
+                answer = 0;
+            }
+            else
+            {
+                answer = Math.Pow(x, 2) - 1;
+            }
             // end
 
             return answer;
@@ -57,7 +68,10 @@
             bool answer = false;
 
             // code here
-
+            if ((x >= 0 && y <= 1 - x && y >= 0) || (x < 0 && y <= 1 + x && y >= 0))
+            {
+                answer = true;
+            }
             // end
 
             return answer;
@@ -68,7 +82,17 @@
             bool answer = true;
 
             // code here
-
+            if (n < 0)
+            {
+                answer = false;
+            }
+            else
+            {
+                if ((double)n % 2.0 == 0)
+                {
+                    answer = false;
+                }
+            }
             // end
 
             return answer;
@@ -78,7 +102,21 @@
             bool answer = false;
 
             // code here
-
+            int a;
+            int min;
+            if (X % 2 == 0)
+            {
+                a = X / 2; // кол-во дней приема чая
+            }
+            else
+            {
+                a = (X / 2) + 1;
+            }
+            min = a * Y; // на сколько минут будет ложиться раньше с помощью чая
+            if (X >= 7 && min >= 240 && min <= 360)
+            {
+                answer = true;
+            }
             // end
 
             return answer;
